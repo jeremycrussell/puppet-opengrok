a4c8fa460df74717514f215715d4a1cd58de7f4c
jeremycrussell <jeremycrussell@gmail.com>
2015-01-16 13:54:15 -0600

    Added SVN repository support.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,4 +1,18 @@
 
+define opengrok::repo($repo_url) {
+  if $repo_url =~ /git/ {
+    opengrok::git::repo { "$name": 
+     git_url => "$repo_url" 
+    }
+  } elsif $repo_url =~ /svn/ {
+    opengrok::svn::repo { "$name":
+      svn_url => "$repo_url"
+    }
+  } else {
+    warning("repository type not implemented.")
+  }
+}
+
 define opengrok::git::repo($git_url) {
   exec {
     "git clone of ${name}" :
@@ -13,5 +27,22 @@ define opengrok::git::repo($git_url) {
       require => [
         File[$opengrok::dirs::base_path],
         Package['git-core']];
+  }
+}
+
+define opengrok::svn::repo($svn_url) {
+  exec {
+    "svn checkout of ${name}" :
+      command => "svn checkout ${svn_url}",
+      cwd     => "${opengrok::dirs::base_path}/source"
+      path    => ['/bin', '/usr/bin'],
+      unless  => "test -d ${opengrok::dirs::base_path}/source/${name}",
+      timeout => 0,
+      notify  => [
+        Serive['tomcat7'],
+        Exec['opengrok-reindex]],
+      require => [
+        File[$opengrok::dirs::base_path],
+        Package['svn']];
   }
 }
