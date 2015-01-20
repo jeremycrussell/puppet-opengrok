5790fe0837eac38c0c1f6d9c05fa03b7d4816a7a
jeremycrussell <jeremycrussell@gmail.com>
2015-01-20 09:55:56 -0600

    Update init.pp
    Added variables for packages and services.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,27 @@
 class opengrok (
   $repos
 ) {
+
+  case $operatingsystem {
+    centos, redhat: { 
+      $gitpkg = "git" 
+      $svnpkg = "subversion"
+      $ctags = "ctags"
+      $tomcatpkg = "tomcat"
+      $tomcatsrvc = "tomcat"
+    }
+    debian, ubuntu: { 
+      $gitpkg = "git-core" 
+      $svnpkg = "svn"
+      $ctags = "exuberant-ctags"
+      $tomcatpkg = ""
+      $tomcatsrvc = "tomcat7"
+    }
+    default: { fail("Unrecognized operating system!") }
+  }
+    
   package {
-    ['exuberant-ctags', 'git-core'] :
+    ["$ctags", "$gitpkg"] :
       ensure => present;
   }
 
@@ -22,7 +41,7 @@ class opengrok (
       command     => '/var/opengrok/bin/opengrok-indexer',
       path        => ['/usr/bin'],
       timeout     => 0,
-      notify      => Service['tomcat7'],
+      notify      => Service["$tomcatsrvc"],
       require     => File['/var/opengrok/bin/opengrok-indexer'];
   }
 
